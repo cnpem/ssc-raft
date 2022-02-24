9bab9fafae3b4361769d3e6dee0c711279e99b64
Eduardo X. Miqueles <eduardo.miqueles@lnls.br>
2022-02-23 21:06:03 -0300

    Updated project

--- build/lib.linux-ppc64le-3.6/sscRaft/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-try:
-    import pkg_resources 
-    __version__ = pkg_resources.require("raft")[0].version
-except:
-    pass
-
-from .rafttypes import *
-from .parallel import *
