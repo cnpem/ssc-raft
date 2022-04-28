b6d9347d3379561784d10f4d1150e160b0402237
paola.ferraz <paola.ferraz@lnls.br>
2022-04-28 16:12:30 -0300

    Added Rebinning code: python, cpu and gpu. GPU has a unknown bug. Add example file.

--- docs/example/rebinning_example.py
+++ docs/example/rebinning_example.py
@@ -30,6 +30,12 @@ dic = {}  # Declare Dictionary
 
 angle = 0 # (Coneradon) Angle in Degrees of the Cone Projection (2D) you want to obtain
 ttype = 'conical' # (Coneradon) Chose type of Radon transform: 'conical' for coneradon, 'parallel' for radon
+dic['nangles'] = size # (Coneradon) Number of angles
+dic['angles'] = (0,360) # (Coneradon) Interval of angles in degree. Always 360 degrees.
+dic['nproc'] = 16 # (Coneradon) Number of processes to devide the computation of projections (angles)
+dic['blocksize'] = 1 # (Coneradon) Block of computations of projections (angles) inside each process. Recomend 1.
+dic['RayPoints'] = 1000 # (Coneradon) Number of integration points of cone ray tracing
+dic['Interpolation'] = 'Nearest' # (Coneradon) String Type of interpolation, 'Nearest' or 'Linear'. Recomend 'Nearest'.
 dic['PhantomSize']  = (1,1,1) # (Coneradon) Tuple of phantom size (Lx,Ly,Lz), where the size interval is [-Lx,Lx], [-Ly,Ly], [-Lz,Lz]
 dic['ShiftPhantom'] = (0.2,0) # (Coneradon) Tuple of phantom shift (sx,sy)
 dic['ShiftRotation'] = (1,0) # (Coneradon) Tuple of rotation center shift (rx,ry)
@@ -38,13 +44,6 @@ dic['Distances'] = (2,1) # (Coneradon/Rebinning) (z1, z2) Distances source/sampl
 dic['Poni'] = (0.5,0) # (Coneradon/Rebinning) Tuple PONI (point of incidence) of central ray at detector (cx,cy)
 dic['DetectorSize'] = (1,1) # (Coneradon/Rebinning) Tuple of detector size (Dx,Dy), where the size interval is [-Dx,Dx], [-Dy,Dy]
 dic['ParDectSize'] = dic['DetectorSize'] # (Coneradon/Rebinning) Tuple of detector size (Lx,Ly), where the size interval is [-Lx,Lx], [-Ly,Ly]
-
-z1,z2 = dic['Distances']
-sx,sy = dic['ShiftPhantom']
-rx,ry = dic['ShiftRotation']
-# Here we need to magnify the shifts of the rotation and pha
-dic['ShiftPhantom'] = (sx*(z2/z1),sy*(z2/z1)) # (Rebinning) Tuple of phantom shift (sx,sy)
-dic['ShiftRotation'] = (rx*(z2/z1),ry*(z2/z1)) # (Rebinning) Tuple of rotation center shift (rx,ry)
 
 
 dic['Type'] = 'cpu' # (Rebinning) String ('cpu','gpu','py') of function type - cpu, gpu, python, respectively - used to compute tomogram (3D). Defauts to 'cpu'.
@@ -55,6 +54,14 @@ dic['gpus'] = [0] # (Rebinning) List of GPU devices used for computation. GPU fu
 # Order of Rebinning input and output: [angles,slices,X]
 
 # ======== Generate Conical Tomogram ===========================================
+
+z1,z2 = dic['Distances']
+sx,sy = dic['ShiftPhantom']
+rx,ry = dic['ShiftRotation']
+# Here we need to magnify the shifts of the rotation and pha
+dic['ShiftPhantom'] = (sx*z2/z1,sy*z2/z1) # (Rebinning) Tuple of phantom shift (sx,sy)
+dic['ShiftRotation'] = (rx*z2/z1,ry*z2/z1) # (Rebinning) Tuple of rotation center shift (rx,ry)
+
 import time
 start_ = time.time()
 
