1bc4f5250a22443db99328e23763387396260311
Larissa Macul Moreno <larissa.moreno@lnls.br>
2023-01-25 10:57:20 +0000

    Update fdk.py - rotation shift

--- sscRaft/geometries/gc/fdk/fdk.py
+++ sscRaft/geometries/gc/fdk/fdk.py
@@ -137,11 +137,77 @@ def normalize_fdk(tomo, flat, dark, padding):
     tomo[np.isinf(tomo)] = 0.0
     tomo[np.isnan(tomo)] = 0.0
 
-    # rotation shift
+    shift = find_rotation_axis_fdk(tomo)
+    if padding < 2*np.abs(shift):
+        padding = 2*shift
+    padd = padding -2*np.abs(shift)
 
     proj = np.zeros((tomo.shape[1], tomo.shape[0], tomo.shape[2]+padding))
-    proj[:,:,padding//2:tomo.shape[2]+padding//2] = np.swapaxes(tomo,0,1)
+
+    if(shift < 0):
+        proj[:,:,padd//2 + 2*np.abs(shift):tomo.shape[2]+padd//2 + 2*np.abs(shift)] = np.swapaxes(tomo,0,1)
+    else:
+        proj[:,:,padd//2:tomo.shape[2]+padd//2] = np.swapaxes(tomo,0,1)
 
     print('Projections ok!')
     print('Projection shape =', proj.shape[0], proj.shape[1], proj.shape[2])
     return proj
+
+def find_rotation_axis_fdk(tomo, nx_search=1000, nx_window=5, nsinos=None):
+    """Searches for the rotation axis index in axis 2 (x variable).
+    It minimizes the symmetry error.
+    Works with parallel, fan and cone beam sinograms (proof: to do).
+    It is assumed the center of rotation is between two pixels. 
+    It might be interesting to implement an alternative where the center is ON a pixel (better suitted for odd number of angles/projections).
+
+    Parameters
+    ----------
+    tomo : 3 dimensional array_like object
+        Raw tomogram.
+        tomo[m, i, j] selects the value of the pixel at the i-row and j-column in the projection m.
+    nx_search : int, optional
+        Width of the search. 
+        If the center of rotation is not in the interval [nx_search-nx//2; nx_search+nx//2] this function will return a wrong result.
+        Default is nx_search=400.
+nx_window : int, optional
+        How much of the sinogram will be used in the axis 2.
+        Default is nx_window=400.
+    nsinos : int or None, optional
+        Number of sinograms to avarege over.
+        Default is None, which results in nsinos = nz//2, where nz = tomo.shape[1].
+
+    Returns
+    -------
+    deviation : int
+        Number of pixels representing the deviation of the center of rotation from the middle of the tomogram (about the axis=2).
+        The integer "deviation + nx//2" is the pixel index which minimizes the symmetry error in the tomogram.
+    
+    Raises
+    ------
+    ValueError
+        If the number of angles/projections is not an even number.
+    """
+    ntheta, nz, nx = tomo.shape
+
+    diff_symmetry = np.ones(2*nx_search) # começando em zero para podermos fazer um gráfico de semilogy.
+
+    if nsinos is None:
+        nsinos = nz//20
+    if nsinos == 0:
+        nsinos = nz
+    
+    for k in range(nsinos):
+        for i in range(2*nx_search):
+            center_idx = (nx//2)-nx_search+i
+
+            sino_esq = tomo[:ntheta//2, k+(nz//2), center_idx-nx_window : center_idx]
+            sino_dir = np.flip(tomo[ntheta//2:, k+(nz//2), center_idx : center_idx+nx_window], axis=1)
+
+            mean_sinos = np.linalg.norm(sino_esq + sino_dir)/2
+            diff_symmetry[i] += np.linalg.norm(sino_esq - sino_dir) / mean_sinos
+    
+    deviation = np.argmin(diff_symmetry) - nx_search
+
+    print('Shift :', deviation)
+
+    return deviation
