Bug on normalization corrected

examples/mogno/mogno_fdk.py
@@ -10,12 +10,12 @@ experiment['z1 [m]'] = 2103*1e-3
 experiment['z1+z2 [m]'] = 2530.08*1e-3
 experiment['detector pixel [m]'] = 3.61*1e-6
 experiment['recon size'] = 2048
-experiment['gpus'] = np.array([0,1,2,3])
+experiment['gpus'] = [0,1,2,3]
 experiment['rings'] = (True,2)
 experiment['normalize'] = (True,True)
 experiment['padding'] = 800
 experiment['shift'] = (True,0)
-experiment['pco detector'] = True
+experiment['detector type'] = 'pco'
 
 
 start_total = time.time()
@@ -32,7 +32,7 @@ start_read = time.time()
 
 # Ler dado HDF5
 data = h5py.File(in_path + in_name, "r")["scan"]["detector"]["data"][:].astype(np.float32)
-flat = h5py.File(in_path + in_name, "r")["scan"]["detector"]["flats"][:].astype(np.float32)[0,:,:]
+flat = h5py.File(in_path + in_name, "r")["scan"]["detector"]["flats"][:].astype(np.float32)
 dark = h5py.File(in_path + in_name, "r")["scan"]["detector"]["darks"][:].astype(np.float32)[0,:,:]
 
 elapsed_read = time.time() - start_read

sscRaft/geometries/gc/fdk/fdk.py
@@ -48,7 +48,7 @@ def fdk(lab, proj, gpus):
 
     libraft.gpu_fdk(lab, recon_p, proj_p, gpus_p, int(ndev), time_p)
 
-    print("Time of execution: \n \n"+"Phantom size"+str(lab.nx)+"\n Nbeta"+str(lab.nbeta)+"\n ngpus"+str(ndev)+"\n              "+str(time))
+    print("Time of execution: \n \n"+"Phantom size"+str(lab.nx)+"\n Nbeta"+str(lab.nbeta)+"\n ngpus"+str(ndev)+"\n"+str(time))
 
     return recon
 
@@ -73,20 +73,22 @@ def reconstruction_fdk( experiment, data, flat = {}, dark = {}):
         *``experiment['z1+z2 [m]']`` (float): Source-detector distance in meters.
         *``experiment['detector pixel [m]']`` (float): Detector pixel size in meters.
         *``experiment['recon size']`` (int): Reconstruction dimension.
-        *``experiment['gpus']`` (ndarray): List of gpus for processing.
+        *``experiment['gpu']`` (ndarray): List of gpus for processing.
         *``experiment['rings']`` (bool,int): Tuple flag for application of rings removal algorithm. (apply, rings block = 2)
         *``experiment['normalize']`` (bool,bool): Tuple flag for normalization of projection data. ( normalize , use log to normalize)
         *``experiment['shift']`` (bool,int): Tuple (is_autoRot, value). Rotation shift automatic corrrection (is_autoRot) and value
         *``experiment['padding']`` (int): Number of elements for horizontal zero-padding.
-        *``experiment['pco detector']`` (bool): If PCO detector, discar fist 11 rows of data.
+        *``experiment['detector type']`` (string): If detector type 'pco', 'mobpix', 'pimegaSi' or 'pimegaCdTe'. If 'pco' discard fist 11 rows of data.
     """
 
     # Set dictionary parameters by default if not already set.
-    dicparams = ('z1 [m]','z1+z2 [m]','detector pixel [m]','recon size','gpus','rings','normalize',
-                'shift','padding','pco detector')
-    defaut = (500e-3,1.0,1.44e-6,data.shape[2],[0],(True,2),(True,True),(True,0),800,True)
+    dicparams = ('z1 [m]','z1+z2 [m]','detector pixel [m]','recon size','gpu','rings','normalize',
+                'shift','padding','detector type')
+    defaut = (500e-3,1.0,1.44e-6,data.shape[2],[0],(True,2),(True,True),(True,0),800,'pco')
     
     SetDictionary(experiment,dicparams,defaut)
+
+    assert experiment['detector type'] in ('pco', 'mobpix', 'pimegaSi', 'pimegaCdTe')
 
     experiment['use log'] = experiment['normalize'][1]
 
@@ -95,31 +97,32 @@ def reconstruction_fdk( experiment, data, flat = {}, dark = {}):
     else:
         data = np.swapaxes(data,0,1)
 
-    D, Dsd = experiment['z1 [m]'], experiment['z1+z2 [m]']
+    recon = data 
+    # D, Dsd = experiment['z1 [m]'], experiment['z1+z2 [m]']
 
-    dh, dv = experiment['detector pixel [m]'], experiment['detector pixel [m]']
-    nh, nv = int(data.shape[2]), int(data.shape[0])
-    h, v = nh*dh/2, nv*dv/2
+    # dh, dv = experiment['detector pixel [m]'], experiment['detector pixel [m]']
+    # nh, nv = int(data.shape[2]), int(data.shape[0])
+    # h, v = nh*dh/2, nv*dv/2
   
-    beta_max, nbeta = 2*np.pi, int(data.shape[1])
-    dbeta = beta_max/nbeta
+    # beta_max, nbeta = 2*np.pi, int(data.shape[1])
+    # dbeta = beta_max/nbeta
 
-    magn = D/Dsd
-    dx, dy, dz = dh*magn, dh*magn, dv*magn
-    nx, ny, nz = int(experiment['recon size']), int(experiment['recon size']), int(experiment['recon size'])
-    x, y, z = dx*nx/2, dy*ny/2, dz*nz/2
+    # magn = D/Dsd
+    # dx, dy, dz = dh*magn, dh*magn, dv*magn
+    # nx, ny, nz = int(experiment['recon size']), int(experiment['recon size']), int(experiment['recon size'])
+    # x, y, z = dx*nx/2, dy*ny/2, dz*nz/2
 
-    lab = Lab(  x = x, y = y, z = z, 
-                dx = dx, dy = dy, dz = dz, 
-                nx = nx, ny = ny, nz = nz, 
-                h = h, v = v, dh = dh, dv = dv, nh = nh, nv = nv, 
-                D = D, Dsd = Dsd, 
-                beta_max = beta_max, dbeta = dbeta , nbeta = nbeta,
-                rings = int(experiment['rings'][0]), rings_block = int(experiment['rings'][1]) )
+    # lab = Lab(  x = x, y = y, z = z, 
+    #             dx = dx, dy = dy, dz = dz, 
+    #             nx = nx, ny = ny, nz = nz, 
+    #             h = h, v = v, dh = dh, dv = dv, nh = nh, nv = nv, 
+    #             D = D, Dsd = Dsd, 
+    #             beta_max = beta_max, dbeta = dbeta , nbeta = nbeta,
+    #             rings = int(experiment['rings'][0]), rings_block = int(experiment['rings'][1]) )
     
-    recon = fdk(lab, data, experiment['gpus'])
+    # recon = fdk(lab, data, np.array(experiment['gpu']))
 
-    print('fdk')
+    # print('fdk')
 
     return recon
 
@@ -128,13 +131,22 @@ def normalize_fdk(tomo, flat, dark, experiment):
     print('Normalize ....')
 
     padding    = experiment['padding']
-    shift      = experiment['shift'][1]
     is_autoRot = experiment['shift'][0]
+    shift      = experiment['shift'][1]
 
-    if experiment['pco detector']:
-        tomo[:,:11,:] = 1.0
-        flat[:11,:]   = 1.0
-        dark[:11,:]   = 0.0
+    if len(dark.shape) == 3:
+            dark = dark[0]
+
+    if experiment['detector type'] == 'pco':
+        dark[:11,:]    = 0.0
+        tomo[:, :11,:] = 1.0 
+            
+        if len(flat.shape) == 2:
+            flat[:11,:]   = 1.0
+        else: 
+            flat[:,:11,:] = 1.0
+
+    # ========= PYTHON NORMALIZATION ================================================================
 
     # flat = flat - dark
     # for i in range(tomo.shape[0]):
@@ -146,24 +158,51 @@ def normalize_fdk(tomo, flat, dark, experiment):
     # tomo[np.isinf(tomo)] = 0.0
     # tomo[np.isnan(tomo)] = 0.0
 
+    # np.save("normalizado2.npy",tomo[0:5,:,:])
+
+    # if is_autoRot:
+    #     shift = find_rotation_axis_fdk(tomo)
+    #     print("Shift automatico:",shift)
+
+    # if padding < 2*np.abs(shift):
+    #     padding = 2*shift
+
+    # padd = padding - 2 * np.abs(shift)
+
+    # print('Shift value and padding:',shift,padd)
+
+    # proj = np.zeros((tomo.shape[1], tomo.shape[0], tomo.shape[2]+padding))
+
+    # if(shift < 0):
+    #     proj[:,:,padd//2 + 2*np.abs(shift):tomo.shape[2]+padd//2 + 2*np.abs(shift)] = np.swapaxes(tomo,0,1)
+    # else:
+    #     proj[:,:,padd//2:tomo.shape[2]+padd//2] = np.swapaxes(tomo,0,1)
+
+
+    # ========= CUDA NORMALIZATION ================================================================
     tomo = correct_projections(tomo, flat, dark, experiment)
 
+    np.save("normalizado.npy",tomo[:,0:5,:])
+
     if is_autoRot:
-        shift = find_rotation_axis_fdk(tomo)
+        shift = find_rotation_axis_fdk(np.swapaxes(tomo,0,1))
+        print("Shift automatico:",shift)
 
     if padding < 2*np.abs(shift):
         padding = 2*shift
 
     padd = padding - 2 * np.abs(shift)
 
-    # proj = np.zeros((tomo.shape[1], tomo.shape[0], tomo.shape[2]+padding))
+    print('Shift value and padding:',shift,padd)
+
     proj = np.zeros((tomo.shape[0], tomo.shape[1], tomo.shape[2]+padding))
 
     if(shift < 0):
-        proj[:,:,padd//2 + 2*np.abs(shift):tomo.shape[2]+padd//2 + 2*np.abs(shift)] = tomo #np.swapaxes(tomo,0,1)
+        proj[:,:,padd//2 + 2*np.abs(shift):tomo.shape[2]+padd//2 + 2*np.abs(shift)] = tomo
     else:
-        proj[:,:,padd//2:tomo.shape[2]+padd//2] = tomo #np.swapaxes(tomo,0,1)
+        proj[:,:,padd//2:tomo.shape[2]+padd//2] = tomo
 
+    # ===============================================================================
     print('Projections ok!')
     print('Projection shape =', proj.shape[0], proj.shape[1], proj.shape[2])
     return proj

sscRaft/tomogram/flatdark.py
@@ -29,7 +29,11 @@ def flatdarkMultiGPU(frames, flat, dark, dic):
         else:
                 nslices = frames.shape[-2]
 
-        nflats = flat.shape[0]
+        if len(flat.shape) == 2:
+                nflats  = 1
+        else:
+                nflats = flat.shape[0]
+
         flat = np.ascontiguousarray(flat.astype(np.float32))
         flatptr = flat.ctypes.data_as(void_p)
 
@@ -49,12 +53,13 @@ def flatdarkMultiGPU(frames, flat, dark, dic):
         else:
                 libraft.flatdarktranspose_block(gpusptr, int32(ngpus), framesptr, flatptr, darkptr, nrays, nslices, nangles, nflats)
 
-        return frames #np.swapaxes(frames,0,1)
+        return frames 
 
 
 def flatdarkGPU(frames, flat, dark, dic):
         
-        ngpus   = dic['gpu'][0]
+        gpus = dic['gpu']     
+        ngpus = len(gpus)
 
         nrays   = frames.shape[-1]
         nangles = frames.shape[0]
@@ -64,7 +69,11 @@ def flatdarkGPU(frames, flat, dark, dic):
         else:
                 nslices = frames.shape[-2]
 
-        nflats = flat.shape[0]
+        if len(flat.shape) == 2:
+                nflats  = 1
+        else:
+                nflats = flat.shape[0]
+
         flat = np.ascontiguousarray(flat.astype(np.float32))
         flatptr = flat.ctypes.data_as(void_p)
 
@@ -84,7 +93,7 @@ def flatdarkGPU(frames, flat, dark, dic):
         else:
                 libraft.flatdarktranspose_gpu(int32(ngpus), framesptr, flatptr, darkptr, nrays, nslices, nangles, nflats)
 
-        return frames #np.swapaxes(frames,0,1)
+        return frames 
 
 
 def correct_projections(frames, flat, dark, dic, **kwargs):
@@ -122,11 +131,11 @@ def correct_projections(frames, flat, dark, dic, **kwargs):
         
         SetDictionary(dic,dicparams,defaut)
 
-        gpus   = dic['gpu']
+        gpus = dic['gpu']
 
         if len(gpus) == 1:
                 output = flatdarkGPU( frames, flat, dark, dic )
         else:
                 output = flatdarkMultiGPU( frames, flat, dark, dic ) 
 
-        return np.swapaxes(output,1,0)
+        return np.swapaxes(output,0,1)