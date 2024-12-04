d7895e3bf59a30b3146bc83f5cd0899ee2e21d40
Alan Peixinho <alan.peixinho@lnls.br>
2024-12-04 16:20:29 -0300

    include source distribution for sdist / fix ninja version due to potential problems in newer versions

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import find_packages
 from skbuild import setup
 
-setup(
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False
-    )
+setup(packages=find_packages(),
+      include_package_data=False,
+      zip_safe=False)
