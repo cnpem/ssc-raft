5c8231f51d694bdf6152258dd53f2ef188598cdc
Paola Ferraz <paola.ferraz@lnls.br>
2025-03-13 10:11:34 -0300

    Stitching test

--- cuda/src/processing/excentric_tomo.cu
+++ cuda/src/processing/excentric_tomo.cu
@@ -446,8 +446,8 @@ __global__ void KJoinX(float* sinogram, const float* temp1, const float* temp2, 
             // sinogram[blockIdx.y*sizex*2 + outdx] = 0 * temp2[blockIdx.y*sizex + 0];
             // sinogram[blockIdx.y*sizex*2 + 2*sizex - 1 - outdx] = 0 * temp1[blockIdx.y*sizex + 0];
 
-            sinogram[blockIdx.y*sizex*2 + outdx] = temp2[blockIdx.y*sizex + 0];
-            sinogram[blockIdx.y*sizex*2 + 2*sizex - 1 - outdx] = temp1[blockIdx.y*sizex + 0];
+            sinogram[blockIdx.y*sizex*2 + outdx] = temp1[blockIdx.y*sizex + 0];
+            sinogram[blockIdx.y*sizex*2 + 2*sizex - 1 - outdx] = temp2[blockIdx.y*sizex + 0];
         }
     }
 }
